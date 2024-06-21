f30d746c0460731dbd27ec7ac77b345079c6f769
samueljuma <samueljuma1024@gmail.com>
2024-06-21 11:22:41 +0300

    db migration

--- app/src/main/java/com/samueljuma/petapp/data/CatDao.kt
+++ app/src/main/java/com/samueljuma/petapp/data/CatDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,4 +14,13 @@ interface CatDao {
 
     @Query("SELECT * FROM Cat")
     fun getCats(): Flow<List<CatEntity>>
+
+    @Update
+    suspend fun update(catEntity: CatEntity)
+
+    //Function doesn't use suspend since
+    // Flow handles asynchronicity and doesn't block the calling thread
+    @Query("SElECT * FROM Cat WHERE isFavorite = 1")
+    fun getFavoriteCats(): Flow<List<CatEntity>>
+
 }
--- app/src/main/java/com/samueljuma/petapp/data/CatDatabase.kt
+++ app/src/main/java/com/samueljuma/petapp/data/CatDatabase.kt
@@ -1,10 +1,15 @@
 package com.samueljuma.petapp.data
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [CatEntity::class], version = 1)
+@Database(
+    entities = [CatEntity::class],
+    version = 2,
+    autoMigrations = [AutoMigration(from = 1, to = 2)]
+)
 @TypeConverters(PetsTypeConverters::class)
 abstract class CatDatabase: RoomDatabase(){
     abstract fun catDao(): CatDao
