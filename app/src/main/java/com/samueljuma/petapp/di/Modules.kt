26a1a8114fe3f0c7257696c4fb753d4a5c7330ec
samueljuma <samueljuma1024@gmail.com>
2024-06-29 14:56:50 +0300

    inspect network

--- app/src/main/java/com/samueljuma/petapp/di/Modules.kt
+++ app/src/main/java/com/samueljuma/petapp/di/Modules.kt
@@ -1,6 +1,9 @@
 package com.samueljuma.petapp.di
 
 import androidx.room.Room
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.RetentionManager
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.samueljuma.petapp.data.CatDatabase
 import com.samueljuma.petapp.data.CatsAPI
@@ -12,6 +15,7 @@ import com.samueljuma.petapp.workers.PetsSyncWorker
 import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.workmanager.dsl.worker
 import org.koin.dsl.module
@@ -32,10 +36,27 @@ val appModules = module {
     // ViewModel
     single { PetsViewModel(get()) }
 
+    single {
+        val chuckerCollector = ChuckerCollector(
+            context = androidContext(), //Android context
+            showNotification = true, //Show notification boolean
+            retentionPeriod = RetentionManager.Period.ONE_HOUR //The period fo which data is kept
+        )
+        val chuckerInterceptor = ChuckerInterceptor.Builder(androidContext())
+            .collector(chuckerCollector)
+            .maxContentLength(250000L)
+            .redactHeaders(emptySet())
+            .alwaysReadResponseBody(false)
+            .build()
+        OkHttpClient.Builder()
+            .addInterceptor(chuckerInterceptor)
+            .build()
+    }
     // Retrofit Instance
     single{
         Retrofit.Builder()
             .addConverterFactory(json.asConverterFactory(contentType = "application/json".toMediaType()))
+            .client(get())
             .baseUrl("https://cataas.com/api/")
             .build()
     }
@@ -56,5 +77,6 @@ val appModules = module {
     single { get<CatDatabase>().catDao() }
 
     worker { PetsSyncWorker(get(), get(), get()) }
+
 
 }
